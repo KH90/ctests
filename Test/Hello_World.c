e627bcea3258773d1005f6e16ef7170c0a4452c4
KH90 <ckhc90c@gmail.com>
2016-06-05 00:53:53 +0200

    Update Hello_World.c

--- Test/Hello_World.c
+++ Test/Hello_World.c
@@ -1,7 +1,7 @@
-#include <stdio.h>                          # Einfügen der stdio Header Datei
+#include <stdio.h>                          // Einfügen der stdio Header Datei
 
-int main(int argc, char *argv[])            # Start des Hauptprogramms
+int main(int argc, char *argv[])            // Start des Hauptprogramms
 {
-  printf("Hallo World!\n");                 # Gibt "Hallo World!" auf dem Bildschirm aus
-  return 0;                                 # Gibt 0 als Rückgabewert zurück
+  printf("Hallo World!\n");                 // Gibt "Hallo World!" auf dem Bildschirm aus
+  return 0;                                 // Gibt 0 als Rückgabewert zurück
 }
